ed8d9f7463042ac2be8cb16e3b4b5a60fad65bcc
iOS18 <ios18@Dato-2.local>
2013-09-30 21:16:59 +0700

    Initial Commit

---
+++ DemoGit04Tests/DemoGit04Tests.m
@@ -0,0 +1,34 @@
+//
+//  DemoGit04Tests.m
+//  DemoGit04Tests
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 iOS18. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoGit04Tests : XCTestCase
+
+@end
+
+@implementation DemoGit04Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
